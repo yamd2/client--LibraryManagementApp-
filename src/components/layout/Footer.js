6dbda517315814dd5784d4539c9864a1957d22f6
Yam Dangar <yamdangar@gmail.com>
2023-02-01 21:03:57 +1100

    footer update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="p-5 text-center bg-dark text-light">
-      &copy; all right reserved 2023 || Made by Prem with 💓
+      &copy; all right reserved 2023 || Made by YAM
     </div>
   );
 };
